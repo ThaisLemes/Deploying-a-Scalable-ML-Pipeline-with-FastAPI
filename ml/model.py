204eedb9ede6662b6a78e65553520ce4264ef014
Thais Lemes <thaislemes@Thaiss-MacBook-Pro.local>
2025-08-03 16:29:50 -0400

    Added the performance on model slices

--- ml/model.py
+++ ml/model.py
@@ -84,7 +84,7 @@ def load_model(path):
     """ Loads pickle file from `path` and returns it."""
     # TODO: implement the function
     with open(path, 'rb') as file: 
-        model = pickle.load(f)
+        model = pickle.load(file)
         return model 
     
 

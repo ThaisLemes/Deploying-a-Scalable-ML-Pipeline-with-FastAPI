91687fe27d54b84690f1378d6bbae8f89db26cc2
Thais Lemes <thaislemes@Thaiss-MacBook-Pro.local>
2025-08-06 21:41:06 -0400

    Test functions

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,47 @@
 import pytest
-# TODO: add necessary import
-
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+import numpy as np 
+from sklearn.ensemble import RandomForestClassifier 
+from ml.model import train_model, compute_model_metrics, inference, predict
+from train_model import X_train, y_train, X_test, y_test
+from ml.data import apply_label
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_model():
     """
-    # add description for the second test
+    Test that model is a instance of Random Forest Classifier
     """
-    # Your code here
-    pass
+    m = train_model(X_train, y_train)
+    assert isinstance(m,RandomForestClassifier), (
+        f"Expected RandomForestClassifier, but returned {type(m). __name__}"
+    )
+    
+    
+
+def test_expected_type():
+    """
+    If any ML functions return the expected type of result.
+    """
+    final_result = predict(X_test)
+    assert isinstance(final_result, np.ndarray), (
+        f" Expected np.ndarray, but returned {type(result).__name__}"
+    )
+    
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_expected_size():
     """
-    # add description for the third test
+    If the training and test datasets have the expected size.
     """
-    # Your code here
-    pass
+    expected = {
+        "X_train" : (1000, 20),
+        "y_train" : (1000, ),
+        "X_test" : (200, 20),
+        "y_test" : (200,),
+
+        for i, expected in expected.items():
+            data = locals()[i]
+        assert data.shape == expected, (
+            f"{i} should have shape {expected}, but returned {data.shape}."
+        )
+        
+    }
